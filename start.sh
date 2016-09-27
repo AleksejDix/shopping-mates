e69ad5dea8a891fe27a37714151b3b5ad41a1328
Aleksej Dix <git@aleksejdix.com>
2016-09-27 11:13:18 +0200

    switch to incremental

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-# bundle exec jekyll serve --profile --incremental  --baseurl ""
-bundle exec jekyll serve  --baseurl ""
+bundle exec jekyll serve --incremental
+# bundle exec jekyll serve  --baseurl ""
